483c1d5c2a809b9b3c78bc2cd0288384063d047c
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-03-04 20:31:26 +0530

    Crate contextAPI.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,11 @@
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+import PropTypes from "prop-types";
+
+const AuthContext = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+AuthContext.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthContext;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import AuthContext from "./context/AuthContext.jsx";
+import TaskContext from "./context/TaskContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <TaskContext>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </TaskContext>
   </StrictMode>
 );
