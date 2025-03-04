ca5cb0753ed9f780ede89751426ab19814385503
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-03-04 21:52:00 +0530

    Change Header UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./css/App.css";
-import Login from "./components/Auth/Login";
-// import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+// import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
 function App() {
   return (
     <>
-      <div className="container m-auto min-h-screen w-full p-6 flex items-center justify-center">
-        <Login />
-        {/* <EmployeeDashboard /> */}
+      <div className="container m-auto flex min-h-screen w-full items-center justify-center p-6">
+        {/* <Login /> */}
+        <EmployeeDashboard />
       </div>
     </>
   );
--- src/others/Header.jsx
+++ src/others/Header.jsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <div className="flex items-center justify-between">
-      <h1 className="text-2xl font-medium">
+    <div className="flex items-center justify-between p-4">
+      <h1 className="text-2xl font-medium text-black">
         Hello <br />
         <span className="text-3xl font-extrabold">Sarthak✌️</span>
       </h1>
-      <button className="rounded-sm bg-red-500 px-5 py-2 text-white">
-        log out
+      <button className="rounded-sm bg-black px-5 py-2 text-white shadow hover:bg-gray-800 cursor-pointer">
+        Log out
       </button>
     </div>
   );
