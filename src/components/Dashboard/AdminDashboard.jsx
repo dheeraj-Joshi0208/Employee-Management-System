b082a239b24209d8a43750d4f1ee55ccb2fcc11f
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-03-08 00:08:42 +0530

    Add Admin form components.

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,61 +1,10 @@
-import Button from "../Button/Button";
+import AdminForm from "../Auth/AdminForm";
 import Header from "./../../others/Header";
 const AdminDashboard = () => {
   return (
     <>
       <Header />
-      <div className="mx-auto mt-5 w-full px-4 sm:px-6 md:px-8">
-        <div className="w-full rounded-lg border border-gray-200 p-4 shadow-lg sm:p-6">
-          <h2 className="mb-4 text-center text-xl font-semibold">
-            Create Task
-          </h2>
-          <form className="flex flex-col gap-4 md:flex-row">
-            <div className="flex-1 space-y-4">
-              <div>
-                <label className="block text-sm font-medium">Task Title</label>
-                <input
-                  type="text"
-                  placeholder="Enter task title"
-                  className="mt-1 w-full rounded-md border border-gray-200 p-2"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium">Description</label>
-                <textarea
-                  placeholder="Detailed description of task (Max 500 words)"
-                  className="mt-1 h-48 w-full rounded-md border border-gray-200 p-2"
-                />
-              </div>
-            </div>
-            <div className="flex-1 space-y-4">
-              <div>
-                <label className="block text-sm font-medium">Date</label>
-                <input
-                  type="date"
-                  className="mt-1 w-full rounded-md border border-gray-200 p-2"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium">Assign To</label>
-                <input
-                  type="text"
-                  placeholder="Enter assignee"
-                  className="mt-1 w-full rounded-md border border-gray-200 p-2"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium">Category</label>
-                <input
-                  type="text"
-                  placeholder="Design, Development, etc..."
-                  className="mt-1 w-full rounded-md border border-gray-200 p-2"
-                />
-              </div>
-              <Button text="Create Task" className="mt-3"/>
-            </div>
-          </form>
-        </div>
-      </div>
+      <AdminForm />
     </>
   );
 };
